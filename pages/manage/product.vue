260865ea9ec002495714e105704ff110a6f5df85
Wasuphat Pudjaika <narayutput@gmail.com>
2022-09-26 20:37:14 +0700

    refactor(AI4F-103): refactor table data

--- pages/manage/product.vue
+++ pages/manage/product.vue
@@ -1,76 +1,45 @@
 <script lang="ts" setup>
 import { useToast } from "vue-toastification";
+import CarbonEdit from "~icons/carbon/edit";
+import CarbonTrashCan from "~icons/carbon/trash-can";
 import LineMdLoadingTwotoneLoop from "~icons/line-md/loading-twotone-loop";
 import UploadIcon from "~icons/carbon/upload";
 import Product from "~~/models/Product";
 
 definePageMeta({
   title: "Ai4FoodSafety - Manage Product",
-  middleware: [
-    "auth"
-  ],
+  middleware: ["auth"],
   canGoBack: true,
-  fallBackRedirect: "/"
+  fallBackRedirect: "/",
 });
+
 const toast = useToast();
-const { api: productApi, getProductByIds } = useProduct();
-const {
-  today,
-  onlyDate,
-  onlyTime
-} = useDate();
+const { getProductById, api: productApi } = useProduct();
+// const { today, onlyDate, onlyTime } = useDate();
 
 const isFetched = ref(false);
 const loading = ref(false);
 const productIds = ref([]);
+const editedId = ref(null);
+const removedId = ref(null);
 const showModal = ref(false);
 const hasResults = ref(false);
-const results = ref([]);
 const perPage = ref(100);
 const currentPage = ref(1);
-
-function reportMixin_setWidthColumn(ws, header, data, fixedLengthHeader = {}) {
-  const objectMaxLength = header.map(h => fixedLengthHeader[h] || h.length);
-
-  for (let i = 0; i < data.length; i++) {
-    header.forEach((key, j) => {
-      if (!fixedLengthHeader[key]) {
-        const valueString = data[i][key];
-        objectMaxLength[j] =
-          objectMaxLength[j] >= valueString.length
-            ? objectMaxLength[j]
-            : valueString.length;
-      }
-    });
-  }
-
-  ws["!cols"] = objectMaxLength.map(obj => ({ width: obj }));
-
-  return ws;
-}
 
 const fetch = async () => {
   if (!isFetched.value) {
     loading.value = true;
+
     hasResults.value = false;
 
     try {
-      const ProductData: Product[] = await productApi().loadAllProduct();
+      const productData: Product[] = await productApi().loadAllProduct();
 
-      results.value = [];
-
-      if (ProductData.length) {
+      if (productData.length) {
         hasResults.value = true;
-        productIds.value = ProductData.map(({ id }) => id);
-        console.debug(ProductData)
-        ProductData.forEach((el, idx) => {
-          results.value.push({
-            ลำดับ: idx + 1,
-            ชื่อสินค้า: el.productName,
-            รหัสสินค้า: el.productCode,
-            รหัสสินค้าสำรอง: el.alternateProductCode
-          });
-        });
+
+        productIds.value = productData.map(({ id }) => id);
       }
     } catch (error) {
       toast.error("ไม่สามารถโหลดข้อมูลสินค้าได้", { timeout: 1000 });
@@ -81,9 +50,38 @@ const fetch = async () => {
   }
 };
 
-const filteredData = computed(
-  () => results.value.filter((_, idx) => {
-    return (idx >= (currentPage.value - 1) * perPage.value) && (idx < currentPage.value * perPage.value);
+const tableFields = computed(() => {
+  return [
+    { key: "order", label: "ลำดับ" },
+    { key: "productCode", label: "รหัสสินค้า" },
+    { key: "alternateProductCode", label: "รหัสสินค้าลำรอง" },
+    { key: "productName", label: "ชื่อสินค้า" },
+    {
+      key: "action",
+      label: "",
+      thClass: "text-center",
+      tdClass: "text-center",
+    },
+  ];
+});
+
+const productData = computed(() => {
+  return productIds.value.map((productId, index) => {
+    const product = getProductById(productId);
+
+    return {
+      order: index + 1,
+      ...product,
+    };
+  });
+});
+
+const filteredData = computed(() =>
+  productData.value.filter((_, idx) => {
+    return (
+      idx >= (currentPage.value - 1) * perPage.value &&
+      idx < currentPage.value * perPage.value
+    );
   })
 );
 
@@ -91,8 +89,11 @@ const onCreateSuccess = async () => {
   await fetch;
 };
 
-onBeforeMount(fetch);
+const promptRemove = (id) => {
+  removedId.value = id;
+};
 
+onBeforeMount(fetch);
 </script>
 
 <template>
@@ -101,14 +102,17 @@ onBeforeMount(fetch);
       <b-row>
         <b-col cols="7">
           <b-row>
-            <h3 class="font-weight-bold">
-              รายการสินค้า
-            </h3>
+            <h3 class="font-weight-bold">รายการสินค้า</h3>
           </b-row>
         </b-col>
+
         <b-col cols="2" />
         <b-col class="d-flex justify-content-center">
-          <b-button class="me-1" variant="outline-primary" @click="showModal = !showModal">
+          <b-button
+            class="me-1"
+            variant="outline-primary"
+            @click="showModal = !showModal"
+          >
             เพิ่มรายการสินค้า
           </b-button>
           <b-button class="me-1" variant="outline-primary" type="submit">
@@ -116,29 +120,66 @@ onBeforeMount(fetch);
           </b-button>
         </b-col>
       </b-row>
+
+      <b-row class="mt-5">
+        <b-col v-if="loading" cols="12" class="text-center">
+          <line-md-loading-twotone-loop :style="{ fontSize: '2em' }" />
+        </b-col>
+
+        <b-col v-if="hasResults">
+          <b-table
+            id="result-table"
+            hover
+            small
+            caption-top
+            responsive
+            :fields="tableFields"
+            :items="filteredData"
+          >
+            <template #cell(action)="{ item }">
+              <b-button variant="link">
+                <CarbonEdit
+                  style="
+                     {
+                      fontsize: '1em';
+                    }
+                  "
+                />
+              </b-button>
+
+              <b-button
+                variant="link"
+                @click="promptRemove(item.id)"
+                class="ms-3 text-danger"
+              >
+                <CarbonTrashCan
+                  style="
+                     {
+                      fontsize: '1em';
+                    }
+                  "
+                />
+              </b-button>
+            </template>
+          </b-table>
+
+          <b-pagination
+            v-model="currentPage"
+            align="center"
+            :total-rows="productData.length"
+            :per-page="perPage"
+            aria-controls="result-table"
+          />
+        </b-col>
+
+        <b-card v-else bg-variant="light">
+          <b-card-text class="text-center"> ไม่พบรายการสินค้า </b-card-text>
+        </b-card>
+      </b-row>
     </b-container>
-    <product-modal-create v-model="showModal" @success="onCreateSuccess">
+
+    <product-modal-create v-model="editedId" @success="onCreateSuccess">
     </product-modal-create>
-    <div v-if="loading" class="col text-center mt-5">
-      <line-md-loading-twotone-loop :style="{ fontSize: '2em' }" />
-    </div>
-    <p>
-      รายการสินค้า | ข้อมูลล่าสุด {{ onlyDate(today()) + ' ' + onlyTime(today()) + ' น.' }}
-    </p>
-    <div v-if="hasResults">
-      <b-table id="result-table" hover small caption-top responsive :items="filteredData" />
-
-      <b-pagination v-model="currentPage" align="center" :total-rows="results.length" :per-page="perPage"
-        aria-controls="result-table" />
-    </div>
-
-    <b-card v-else bg-variant="light">
-      <b-card-text class="text-center">
-        ไม่พบรายการสินค้า
-      </b-card-text>
-    </b-card>
   </div>
 </template>
-<style module>
-
-</style>
+<style module></style>
