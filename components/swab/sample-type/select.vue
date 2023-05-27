947d10d59e9a8c8fa26bd78eaaa15f60315cc01e
Wasuphat Pudjaika <narayutput@gmail.com>
2023-05-27 23:39:40 +0700

    feat(AI4F-170): include swab-sample-type-select component to swab-product-history-form component

--- components/swab/sample-type/select.vue
+++ components/swab/sample-type/select.vue
@@ -2,26 +2,35 @@
 import { useToast } from "vue-toastification";
 import SwabSampleType from "~~/models/SwabSampleType";
 import vSelect from "vue-select";
+import { requiredIf } from "@vuelidate/validators";
 
 export type SwabSampleTypeSelectData = {
   id: string;
 };
 
 export interface Props {
+  required?: boolean;
   clearable?: boolean;
   disabled?: boolean;
+  label?: string;
+  showLabel?: boolean;
   once?: boolean;
   lazy?: boolean;
   modelValue?: SwabSampleTypeSelectData | null;
 }
+
+const attrs = useAttrs();
 
 const toast = useToast();
 
 const { api: swabSampleTypeApi, getAllSwabSampleTypes } = useSwab();
 
 const props = withDefaults(defineProps<Props>(), {
+  required: false,
   clearable: false,
   disabled: false,
+  label: "ประเภทตัวอย่าง",
+  showLabel: false,
   once: false,
   lazy: false,
   modelValue: null,
@@ -47,6 +56,25 @@ const modelValue = computed({
 
   set: (value) => emit("update:modelValue", value),
 });
+
+const validationRules = {
+  modelValue: {
+    requiredIfPropsRequired: requiredIf(() => props.required),
+    $lazy: true,
+  },
+};
+
+const { isInvalid, isFormInvalid } = useValidation(validationRules, {
+  modelValue,
+});
+
+const swabSampleTypeRequiredState = computed(() =>
+  isFormInvalid("modelValue", ["requiredIfPropsRequired"])
+);
+
+const formGroupLabel = computed(() => (props.showLabel ? props.label : ""));
+
+const formGroupLabelClass = computed(() => (!props.showLabel ? "p-0" : ""));
 
 const fetch = async () => {
   if (props.once && isFetched.value) return;
@@ -76,23 +104,38 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <v-select
-    v-model="modelValue"
-    :options="SwabSampleTypeOptions"
-    label="swabSampleTypeName"
-    :loading="loading"
-    :clearable="clearable"
-    :disabled="disabled"
-    :reduce="({ id }) => ({ id })"
-    deselect-from-dropdown
-    @open="fetch"
+  <b-form-group
+    label-for="swabSampleTypeName"
+    :label="formGroupLabel"
+    :label-class="formGroupLabelClass"
+    :state="swabSampleTypeRequiredState"
+    v-bind="{ ...attrs }"
   >
-    <template #selected-option="{ swabSampleTypeName }">
-      {{ swabSampleTypeName }}
-    </template>
+    <v-select
+      v-model="modelValue"
+      :options="SwabSampleTypeOptions"
+      label="swabSampleTypeName"
+      :loading="loading"
+      :clearable="clearable"
+      :disabled="disabled"
+      :reduce="({ id }) => ({ id })"
+      deselect-from-dropdown
+      @open="fetch"
+    >
+      <template #selected-option="{ swabSampleTypeName }">
+        {{ swabSampleTypeName }}
+      </template>
 
-    <template #option="{ swabSampleTypeName }">
-      {{ swabSampleTypeName }}
-    </template>
-  </v-select>
+      <template #option="{ swabSampleTypeName }">
+        {{ swabSampleTypeName }}
+      </template>
+    </v-select>
+
+    <b-form-invalid-feedback
+      v-if="isInvalid"
+      :state="swabSampleTypeRequiredState"
+    >
+      กรุณาเลือก{{ label }}
+    </b-form-invalid-feedback>
+  </b-form-group>
 </template>
